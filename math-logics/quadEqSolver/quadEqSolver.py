c32c0cf4c6a838f92e010e353a7529fea9cd3b29
Lakshyaveer Turna <144465722+im-lakshyaveerturna@users.noreply.github.com>
2024-07-21 13:46:57 +0530

    Update quadEqSolver.py

--- math-logics/quadEqSolver/quadEqSolver.py
+++ math-logics/quadEqSolver/quadEqSolver.py
@@ -1,23 +1,29 @@
 #Quadratic equation solver
 #Displays only integral roots
-#Range limit: [-1000,1000] (c'mon, it's a python code with double for loops, you know... )
+#Range limit: [-1000,1000] (speed limit maybe compromised significantly for longer ranges)
 
 
 a = int(input('a: '))
 b = int(input('b: '))
 c = int(input('c: '))
+
 count = 0
+add = b
+product = a*c
 
 for i in range(-1000,1001):
-    for n in range(-1000,1001):
-        if int(i*n) == int(a*c) and int(i + n) == b and count == 0:
-            print('Roots:',str(i)+',',n)
-            count += 1
+    if count == 0:
+        for n in range(-1000,1001):
             
+            if (i*n) == product and (i + n) == add:
+                print('Roots:', str(i) + ',', n)
+                count += 1
+        
             
 if count == 0:
     print('No integral number roots')
             
+     
     
             
 
